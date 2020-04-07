2a79ae097b91e23cbe9b4e7b0be445b409634267
iicefy <chawitmobile@gmail.com>
2020-04-07 22:01:56 +0700

    create new file

---
+++ Screens/testgit2.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const testgit2 = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default testgit2;
